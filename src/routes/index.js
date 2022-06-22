0333da416f0f447cac67b8117cc6d0fa6f5a2eff
andrei <andre4acc@gmail.com>
2022-06-23 00:45:29 +0700

    add login and register

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const Router = require('express').Router();
 
 const pingRouter = require('./ping');
+const authRouter = require('./auth')
 
 Router.use('/ping', pingRouter);
+Router.use('/auth', authRouter)
 
 module.exports = Router;
