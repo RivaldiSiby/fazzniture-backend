9fd08fe9351f1fc8b913c470ee848b1ad4ec5f66
Rivaldi Siby <rivaldisiby@gmail.com>
2022-06-23 19:43:05 +0800

    fix bug regis and login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,49 +1,49 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const {getPassByEmail, signUp} = require('../models/auth')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { getPassByEmail, signUp } = require("../models/auth");
 
-const register = async (req, res)=>{
-    try {
-        const password = req.body.password
-        const salt = await bcrypt.genSalt()
-        const hashPassword = await bcrypt.hash(password, salt)
-        const result = await signUp(req.body, hashPassword)
-        res.status(200).json({
-            msg : "Register Succes",
-            data : result
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            msg : "Register Failed",
-            error
-        })
+const register = async (req, res) => {
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt();
+    const hashPassword = await bcrypt.hash(password, salt);
+    const result = await signUp(req.body, hashPassword);
+    res.status(200).json({
+      msg: "Register Succes",
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "Register Failed",
+      error,
+    });
+  }
+};
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const payload = await getPassByEmail(email);
+    const result = await bcrypt.compare(password, payload.password);
+    if (!result) {
+      return res.status(400).json({
+        msg: "Wrong email or password",
+      });
     }
-}
-const login = async (req, res)=>{
-    try {
-        const {email, password} = req.body
-        const payload = await getPassByEmail(email)
-        const result = await bcrypt.compare(password, payload.password)
-        if(!result){
-            return res.status(400).json({
-                msg : "Wrong email or password"
-            })
-        }
-        const token = jwt.sign(payload, process.env.JWT_SECRET, {
-            expiresIn : "1000s"
-        })
-        res.status(200).json({
-            msg : "Login Succes",
-            token
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            msg : "Login Failed",
-            error
-        })
-    }
-}
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1000s",
+    });
+    res.status(200).json({
+      msg: "Login Succes",
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "Login Failed",
+      error,
+    });
+  }
+};
 
-module.exports = {register, login}
+module.exports = { register, login };
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,41 +1,46 @@
-const jwt = require('jsonwebtoken')
-const db = require('../config/db')
+const jwt = require("jsonwebtoken");
+const db = require("../config/db");
 
-const chekDuplicateEmail = async (req, res, next)=>{
-    try {
-        const checkEmail = db.query(`SELECT * FROM users where email = ${req.body.email}`)
-        if(!req.body.email){
-            return res.status(400).json({
-                msg : "Please input valid email"
-            })
-        }
-        if(checkEmail === 0){
-            return res.status(400).json({
-                msg : "Email is already used"
-            })
-        }
-    } catch (error) {
-        res.status(400).json({
-            msg : "Register failed",
-            error
-        })
-        console.log(error);
+const chekDuplicateEmail = async (req, res, next) => {
+  try {
+    const checkEmail = await db.query("SELECT * FROM users where email = $1", [
+      req.body.email,
+    ]);
+    if (!req.body.email) {
+      return res.status(400).json({
+        msg: "Please input valid email",
+      });
     }
-}
+    if (checkEmail === 0) {
+      return res.status(400).json({
+        msg: "Email is already used",
+      });
+    }
+    next();
+  } catch (error) {
+    res.status(400).json({
+      msg: "Register failed",
+      error,
+    });
+    console.log(error);
+  }
+};
 
-const verifyToken = (req, res, next)=>{
-    const bearerToken = req.header('Authorization')
-    if(!bearerToken) return res.status(403).json({
-        msg : "You need to sign in"
-    })
-    const oldtoken = bearerToken.split(" ")[1]
-    jwt.verify(oldtoken, process.env.JWT_SECRET, (err, payload)=>{
-        if (err && err.name === "TokenExpiredError") return res.status(401).json({
-            msg : "You need to sign in again"
-        })
-        req.userPayload = payload
-        next()
-    })
-}
+const verifyToken = (req, res, next) => {
+  const bearerToken = req.header("Authorization");
+  if (!bearerToken)
+    return res.status(403).json({
+      msg: "You need to sign in",
+    });
+  const oldtoken = bearerToken.split(" ")[1];
+  jwt.verify(oldtoken, process.env.JWT_SECRET, (err, payload) => {
+    if (err && err.name === "TokenExpiredError")
+      return res.status(401).json({
+        msg: "You need to sign in again",
+      });
+    req.userPayload = payload;
+    next();
+  });
+};
 
-module.exports = {verifyToken, chekDuplicateEmail}
+module.exports = { verifyToken, chekDuplicateEmail };
