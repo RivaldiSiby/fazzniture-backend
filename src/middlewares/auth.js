00540d9b216d58091efcdfbb883fce1b37c9e968
Rivaldi Siby <rivaldisiby@gmail.com>
2022-06-24 04:12:46 +0800

    deploy

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,27 +1,29 @@
 const jwt = require("jsonwebtoken");
 const db = require("../config/db");
 
-const chekDuplicateEmail = async (req, res, next)=>{
-    try {
-        const email = req.body.email
-        const checkEmail = await db.query(`SELECT * FROM users where email = $1`, [email])
-        if(!req.body.email){
-            return res.status(400).json({
-                msg : "Please input valid email"
-            })
-        }
-        if(checkEmail.rowCount > 0){
-            return res.status(400).json({
-                msg : "Email is already used"
-            })
-        }
-        next()
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            msg : "Register failed",
-            error
-        })
+const chekDuplicateEmail = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const checkEmail = await db.query(`SELECT * FROM users where email = $1`, [
+      email,
+    ]);
+    if (!req.body.email) {
+      return res.status(400).json({
+        msg: "Please input valid email",
+      });
+    }
+    if (checkEmail.rowCount > 0) {
+      return res.status(400).json({
+        msg: "Email is already used",
+      });
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      msg: "Register failed",
+      error,
+    });
 
     console.log(error);
   }
