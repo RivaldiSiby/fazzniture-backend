8ce754dee934ea0b3d58f17851dcea31bac3e91a
RSiby <86668225+RivaldiSiby@users.noreply.github.com>
2022-06-24 12:16:04 +0800

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const pingRouter = require('./ping');
 const usersRoutes = require('./users');
 const authRouter = require('./auth')
 const transactionRouter = require('./transactions')
+const productRouter = require('./product');
 
 Router.use('/ping', pingRouter);
 Router.use('/users', usersRoutes);
 Router.use('/auth', authRouter)
+Router.use('/product', productRouter);
 Router.use('/transaction', transactionRouter)
 
 module.exports = Router;
