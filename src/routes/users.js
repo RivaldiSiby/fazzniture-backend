9735c6849626426f04fbe455af6f89326a6e885b
Rivaldi Siby <rivaldisiby@gmail.com>
2022-06-25 12:47:49 +0800

    fix patchProfile

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,7 @@ const { upload } = require("../middlewares/upload");
 Router.post("/", userController.postUsersControllers);
 Router.get("/", middleware.verifyToken, userController.findUsersControllers);
 Router.patch(
-  "/:id",
+  "/",
   upload,
   middleware.verifyToken,
   userController.patchUsersControllers
