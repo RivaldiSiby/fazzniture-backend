0ea4eaecf18025094eb52dd2139dc779daf4d506
JalalDA <48475526+JalalDA@users.noreply.github.com>
2022-06-28 02:51:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {redisCon} = require('./src/config/redis')
 
 const server = express();
 const PORT = process.env.PORT || 8080;
-console.log(rediscon())
+console.log(redisCon())
 db.connect()
    .then(() => {
       server.use(express.json());
