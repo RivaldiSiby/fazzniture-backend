cb312cfc26b714727a012ec29e83ce9223884f71
RSiby <86668225+RivaldiSiby@users.noreply.github.com>
2022-06-27 23:27:01 +0800

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -8,5 +8,7 @@ const { validateRegister, validateLogin } = require('../middlewares/validasi/val
 Router.post('/login', validateLogin, authControllers.login);
 Router.post('/register', validateRegister, middleware.chekDuplicateEmail, authControllers.register);
 Router.delete('/logout', authControllers.logout);
+Router.post('/forgot-password', authControllers.forgotPassword)
+Router.put('/reset-password', authControllers.resetPassword)
 
 module.exports = Router;
